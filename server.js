64323ed2214a8ad50bbe888a46fdcd6ad44358fd
Jaejin Lee <bertismrpdl@gmail.com>
2017-07-16 16:14:12 -0400

    Remote reset and statshed

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-// require("./utilities/filelist");
-
 app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port);
