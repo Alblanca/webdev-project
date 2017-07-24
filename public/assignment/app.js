4510763f0e3b0e42306564f6be1cacda82c3361a
Jaejin Lee <bertismrpdl@gmail.com>
2017-07-23 20:34:36 -0400

    Initial Refactoring for assignment 3

---
+++ public/assignment/app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by berti on 7/23/2017.
+ */
+var app = angular.module("WebAppMaker", []);
+app.controller("loginController", loginController);
+
+function loginController($scope) {
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ]
+
+    $scope.login = function (user) {
+        for (var u in users) {
+            var _user = users[u];
+            if (_user.username == user.username && _user.password == user.password) {
+                $scope.welcomeUser = _user;
+            }
+        }
+    }
+}
