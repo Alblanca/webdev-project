6602d7a413db7e2134ef34abef68bd468b4deb72
Jaejin Lee <bertismrpdl@gmail.com>
2017-08-17 19:50:08 -0400

    Cookie set up

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var app = require('./express');
 var express = app.express;
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+
+app.use(session({
+    secret: 'aa',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
