9140a3a1dedf6191e4439304b470c55cb59b85da
Jaejin Lee <bertismrpdl@gmail.com>
2017-08-18 07:21:02 -0400

    db configured

--- assignment/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by berti on 7/24/2017.
- */
-// require("./user.service.server");
-// require("./website.service.server");
-// require("./page.service.server");
-// require("./widget.service.server");
-// var db = require("./models/database");
-
