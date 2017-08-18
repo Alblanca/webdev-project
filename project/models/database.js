cdf1343aac970ec12ffae213846396551a867e15
Jaejin Lee <bertismrpdl@gmail.com>
2017-08-18 07:21:51 -0400

    db configured

--- project/models/database.js
+++ project/models/database.js
@@ -7,7 +7,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds149613.mlab.com:49613/heroku_x654gt9k/overhub_db'; // user yours
+    connectionString += '@ds149613.mlab.com:49613/heroku_x654gt9k'; // user yours
 }
 
 
